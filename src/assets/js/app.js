7f536b472a04bab063207724166e5182fdf420d1
Jannie Theunissen <jannie@onesheep.org>
2020-04-01 09:27:00 +0200

    search working

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,13 +6,20 @@ window.store = function() {
     items: [],
     query: null,
 
+    filteredItems() {
+      return this.items.filter(s => s.title.toLowerCase().indexOf(this.query.toLowerCase()) !== -1);
+    },
+
     init() {
       Object.entries(data).forEach(([key, value]) => {
         this.sections.push(key);
-        console.log(value);
+        for (let item of value) {
+          this.items.push(item);
+        }
       });
 
-      this.sections.forEach(s => console.log(s));
+      //   this.sections.forEach(s => console.log(s));
+      //   this.items.forEach(s => console.log(s.title));
       //   console.log(window.data["Starters Guide"][0].title);
     }
   };
